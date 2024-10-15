ece045c045d300e30804c584364c475e58718c12
Kaustub12 <Kaustub12@yahoo.com>
2024-10-15 09:28:17 +0300

    initial commit

---
+++ SeleniumJavaProject/src/main/java/SeleniumJavaAutomation/SeleniumJavaProject/App.java
@@ -0,0 +1,13 @@
+package SeleniumJavaAutomation.SeleniumJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
